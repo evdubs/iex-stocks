b5776249fe6f106c289cc48485699b63c3ad3f2d
evdubs <evdubs@gmail.com>
2019-07-24 18:45:56 -1000

    pass along token to splits extract

--- splits-extract-transform-load.sh
+++ splits-extract-transform-load.sh
@@ -4,7 +4,7 @@ dir=$(dirname "$0")
 today=$(date "+%F")
 current_year=$(date "+%Y")
 
-racket ${dir}/splits-extract.rkt -p "$1"
+racket ${dir}/splits-extract.rkt -p "$1" -t "$2"
 racket ${dir}/splits-transform-load.rkt -p "$1"
 
 7zr a /var/tmp/iex/splits/${current_year}.7z /var/tmp/iex/splits/${today}
