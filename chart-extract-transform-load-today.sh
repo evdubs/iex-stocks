5c0581f4804aca5469c54f871ca57a7858bef837
evdubs <evdubs@gmail.com>
2019-10-21 17:23:59 -1000

    add chart etl script for today

---
+++ chart-extract-transform-load-today.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+dir=$(dirname "$0")
+today=$(date "+%F")
+today_year=$(date "+%Y")
+
+racket ${dir}/chart-extract.rkt -d ${today} -p "$1" -t "$2"
+racket ${dir}/chart-transform-load.rkt -d ${today} -p "$1"
+
+7zr a /var/tmp/iex/chart/${today_year}.7z /var/tmp/iex/chart/${today}
