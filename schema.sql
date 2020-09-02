7077e6ea58ca8b5b379807482674d8bd8d40b0d5
evdubs <evdubs@gmail.com>
2020-09-01 15:06:10 -1000

    add adjusted stock price function

--- schema.sql
+++ schema.sql
@@ -118,3 +118,62 @@ CREATE TABLE iex.split
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
+
+CREATE OR REPLACE FUNCTION iex.split_adjusted_chart(arg_act_symbol text, arg_start_date date, arg_end_date date)
+ RETURNS TABLE(act_symbol text, date date, open numeric, high numeric, low numeric, close numeric, volume numeric)
+ LANGUAGE sql
+AS $function$
+select
+  act_symbol,
+  date,
+  trunc(open / mul(split_ratio), 4) as open,
+  trunc(high / mul(split_ratio), 4) as high,
+  trunc(low / mul(split_ratio), 4) as low,
+  trunc(close / mul(split_ratio), 4) as close,
+  trunc(volume * mul(split_ratio), 4) as volume
+from
+  (select
+    c.act_symbol,
+    c.date,
+    c.open,
+    case 
+      when c.high is null and c.open >= c.close then c.open
+      when c.high is null and c.open < c.close then c.close
+      else c.high
+    end as high,
+    case 
+      when c.low is null and c.open >= c.close then c.close
+      when c.low is null and c.open < c.close then c.open
+      else c.low
+    end as low,
+    c.close,
+    case
+      when c.volume is null then 0
+      else c.volume
+    end as volume,
+    s.split_ratio
+  from
+    iex.chart c
+  left join
+    (select
+      act_symbol,
+      ex_date as date,
+      to_factor / for_factor as split_ratio
+    from
+      iex.split
+    where
+      act_symbol = arg_act_symbol and
+      ex_date >= arg_start_date) s
+  on
+    c.act_symbol = s.act_symbol and
+    c.date < s.date
+  where
+    c.act_symbol = arg_act_symbol and
+    c.date >= arg_start_date and
+    c.date <= arg_end_date) as adjusted_chart
+group by
+  act_symbol, date, open, high, low, close, volume
+order by
+  act_symbol, date;
+$function$
+;
