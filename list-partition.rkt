239657b9de441b42695be00a21a37463cd175dcd
evdubs <1124030+evdubs@users.noreply.github.com>
2020-12-29 00:32:01 -1000

    make sure we don't append an empty list to our partitioned lists

--- list-partition.rkt
+++ list-partition.rkt
@@ -5,6 +5,6 @@
 (provide list-partition)
 
 (define (list-partition l period step)
-  (if (> period (length l)) (list l)
+  (if (>= period (length l)) (list l)
       (append (list (take l period))
               (list-partition (drop l step) period step))))
