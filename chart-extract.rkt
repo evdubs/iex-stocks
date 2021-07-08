9e616d55cf82298e9749373ccb4285d7b3e58a1a
evdubs <1124030+evdubs@users.noreply.github.com>
2021-07-08 09:18:19 -1000

    spawn a thread when downloading a symbol range. this hopefully lets the processing continue when network outages occur

--- chart-extract.rkt
+++ chart-extract.rkt
@@ -115,7 +115,7 @@ order by
 
 (define delays (map (λ (x) (* delay-interval x)) (range 0 (length grouped-symbols))))
 
-(with-task-server (for-each (λ (l) (schedule-delayed-task (λ () (download-chart (first l)))
+(with-task-server (for-each (λ (l) (schedule-delayed-task (λ () (thread (λ () (download-chart (first l)))))
                                                           (second l)))
                             (map list grouped-symbols delays))
   ; add a final task that will halt the task server
