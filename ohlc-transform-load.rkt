fbfaa3ca4c77209e4faf85b4818759005731d2ec
evdubs <1124030+evdubs@users.noreply.github.com>
2021-08-25 10:45:32 -1000

    use gregor for dates/times. write to iex.chart (maybe this should be configurable). check timestamps to make sure the prices are for the correct date

--- ohlc-transform-load.rkt
+++ ohlc-transform-load.rkt
@@ -1,17 +1,18 @@
 #lang racket/base
 
 (require db
+         gregor
+         gregor/period
          json
          racket/cmdline
          racket/port
          racket/sequence
          racket/string
-         srfi/19 ; Time Data Types and Procedures
          threading)
 
 (define base-folder (make-parameter "/var/tmp/iex/ohlc"))
 
-(define folder-date (make-parameter (current-date)))
+(define folder-date (make-parameter (today)))
 
 (define db-user (make-parameter "user"))
 
@@ -27,7 +28,7 @@
                          (base-folder folder)]
  [("-d" "--folder-date") date
                          "IEX Stocks OHLC folder date. Defaults to today"
-                         (folder-date (string->date date "~Y-~m-~d"))]
+                         (folder-date (iso8601->date date))]
  [("-n" "--db-name") name
                      "Database name. Defaults to 'local'"
                      (db-name name)]
@@ -40,28 +41,31 @@
 
 (define dbc (postgresql-connect #:user (db-user) #:database (db-name) #:password (db-pass)))
 
-(parameterize ([current-directory (string-append (base-folder) "/" (date->string (folder-date) "~1") "/")])
+(parameterize ([current-directory (string-append (base-folder) "/" (date->iso8601 (folder-date)) "/")])
   (for ([p (sequence-filter (λ (p) (string-contains? (path->string p) ".json")) (in-directory))])
-    (let ([file-name (string-append (base-folder) "/" (date->string (folder-date) "~1") "/" (path->string p))]
+    (let ([file-name (string-append (base-folder) "/" (date->iso8601 (folder-date)) "/" (path->string p))]
           [ticker-range (string-replace (path->string p) ".json" "")])
       (call-with-input-file file-name
         (λ (in)
           (with-handlers ([exn:fail? (λ (e) (displayln (string-append "Failed to process "
                                                                       ticker-range
                                                                       " for date "
-                                                                      (date->string (folder-date) "~1")))
+                                                                      (date->iso8601 (folder-date))))
                                        (displayln ((error-value->string-handler) e 1000))
                                        (rollback-transaction dbc))])
             (start-transaction dbc)
             (~> (port->string in)
                 (string->jsexpr _)
                 (hash-for-each _ (λ (symbol ohlc-hash)
-                                   (let ([open (hash-ref (hash-ref ohlc-hash 'ohlc) 'open)]
-                                         [high (hash-ref (hash-ref ohlc-hash 'ohlc) 'high)]
-                                         [low (hash-ref (hash-ref ohlc-hash 'ohlc) 'low)]
-                                         [close (hash-ref (hash-ref ohlc-hash 'ohlc) 'close)])
-                                     (query-exec dbc "
-insert into iex.ohlc (
+                                   (let* ([open (hash-ref (hash-ref ohlc-hash 'ohlc) 'open)]
+                                          [high (hash-ref (hash-ref ohlc-hash 'ohlc) 'high)]
+                                          [low (hash-ref (hash-ref ohlc-hash 'ohlc) 'low)]
+                                          [close (hash-ref (hash-ref ohlc-hash 'ohlc) 'close)]
+                                          [close-unix-millis (hash-ref (hash-ref (hash-ref ohlc-hash) 'close) 'time)]
+                                          [close-date (->date (+period (datetime 1970) (period [milliseconds close-unix-millis])))])
+                                     (cond [(date=? (folder-date) close-date)
+                                            (query-exec dbc "
+insert into iex.chart (
   act_symbol,
   date,
   open,
@@ -89,12 +93,12 @@ insert into iex.ohlc (
   end
 ) on conflict (act_symbol, date) do nothing;
 "
-                                                 (symbol->string symbol)
-                                                 (date->string (folder-date) "~1")
-                                                 (if (hash-empty? open) "" (real->decimal-string (hash-ref open 'price) 4))
-                                                 (if (equal? high 'null) "" (real->decimal-string high 4))
-                                                 (if (equal? low 'null) "" (real->decimal-string low 4))
-                                                 (if (hash-empty? close) "" (real->decimal-string (hash-ref close 'price) 4)))))))
+                                                        (symbol->string symbol)
+                                                        (date->iso8601 close-date)
+                                                        (if (hash-empty? open) "" (real->decimal-string (hash-ref open 'price) 4))
+                                                        (if (equal? high 'null) "" (real->decimal-string high 4))
+                                                        (if (equal? low 'null) "" (real->decimal-string low 4))
+                                                        (if (hash-empty? close) "" (real->decimal-string (hash-ref close 'price) 4)))])))))
             (commit-transaction dbc)))))))
 
 (disconnect dbc)
